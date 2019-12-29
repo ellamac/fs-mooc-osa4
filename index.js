3a4aaec3b3a34c1afc4fb67dbbf692c0568e0994
Ella Mäkelä <ella@makela.co>
2019-12-29 18:02:50 +0200

    models/blog.js

--- index.js
+++ index.js
@@ -5,19 +5,7 @@ const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
-
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-});
-
-const Blog = mongoose.model('Blog', blogSchema);
-
-const mongoUrl = process.env.MONGODB_URI;
-
-mongoose.connect(mongoUrl, { useNewUrlParser: true });
+const Blog = require('./models/blog');
 
 app.use(cors());
 app.use(bodyParser.json());
