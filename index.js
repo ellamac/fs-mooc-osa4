c44c7d294821aa4b6f075e170312185685b6c7d5
Ella Mäkelä <ella@makela.co>
2019-12-29 18:09:04 +0200

    controllers/blogs.js

--- index.js
+++ index.js
@@ -6,23 +6,12 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const Blog = require('./models/blog');
+const blogsRouter = require('./controllers/blogs');
 
 app.use(cors());
 app.use(bodyParser.json());
 
-app.get('/api/blogs', (request, response) => {
-  Blog.find({}).then(blogs => {
-    response.json(blogs);
-  });
-});
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body);
-
-  blog.save().then(result => {
-    response.status(201).json(result);
-  });
-});
+app.use('/api/blogs', blogsRouter);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
